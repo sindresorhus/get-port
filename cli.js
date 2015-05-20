3f9ecdbb3a06e81350ecc1ff9489274c1ac82f55
Arthur Verschaeve <contact@arthurverschaeve.be>
2015-05-20 18:46:00 +0200

    Use `meow` in the cli

--- cli.js
+++ cli.js
@@ -1,29 +1,15 @@
 #!/usr/bin/env node
 'use strict';
-var pkg = require('./package.json');
+var meow = require('meow');
 var getPort = require('./');
-var argv = process.argv.slice(2);
 
-function help() {
-	console.log([
-		'',
-		'  ' + pkg.description,
-		'',
-		'  Example',
-		'    get-port',
-		'    51402'
-	].join('\n'));
-}
-
-if (argv.indexOf('--help') !== -1) {
-	help();
-	return;
-}
-
-if (argv.indexOf('--version') !== -1) {
-	console.log(pkg.version);
-	return;
-}
+meow({
+	help: [
+		'Example',
+		'  $ get-port',
+		'  51402'
+	].join('\n')
+});
 
 getPort(function (err, port) {
 	if (err) {
