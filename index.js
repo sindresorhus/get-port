038b69c29e2a7f14e7e1f5842c8cdc42415784d8
DJ Madeira <dmadeira@medallia.com>
2017-03-31 16:22:55 -0400

    Preferred port

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 'use strict';
 const net = require('net');
 
-module.exports = () => new Promise((resolve, reject) => {
+const getPort = port => new Promise((resolve, reject) => {
 	const server = net.createServer();
 
 	server.unref();
 	server.on('error', reject);
 
-	server.listen(0, () => {
+	server.listen(port, () => {
 		const port = server.address().port;
 		server.close(() => {
 			resolve(port);
 		});
 	});
 });
+
+module.exports = preferredPort => preferredPort ?
+	getPort(preferredPort).catch(() => getPort(0)) :
+	getPort(0);
