65df48b4c6678c96d1568bb843a43e12db40c132
nagen <nagendran010@gmail.com>
2018-05-03 22:17:19 -0400

    accept port as an array

--- index.js
+++ index.js
@@ -6,22 +6,41 @@ const getPort = options => new Promise((resolve, reject) => {
 	// TODO: Remove this in the next major version
 	if (typeof options === 'number') {
 		options = {
-			port: options
+			port: [options]
+		};
+	} else if (typeof options.port === 'number') {
+		options = {
+			port: [options.port]
 		};
 	}
 
+	options.port.forEach( (e, index) => {
+		isAvailable(e, res => {
+			if(res === false){
+				if(index !== options.port.length-1) return;
+				reject();
+			}
+
+			resolve(res);
+		});
+	});
+});
+
+const isAvailable = (options, callback) => {
 	const server = net.createServer();
 
 	server.unref();
-	server.on('error', reject);
+	server.on('error', () => {
+		callback(false);
+	});
 
 	server.listen(options, () => {
 		const port = server.address().port;
 		server.close(() => {
-			resolve(port);
+			callback(port);
 		});
 	});
-});
+};
 
 module.exports = options => options ?
 	getPort(options).catch(() => getPort(0)) :
