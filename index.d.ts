a10e3be92bf4dc469247948e0d391755c60e2a55
Sindre Sorhus <sindresorhus@gmail.com>
2019-02-20 15:01:04 +0700

    Update index.test-d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,22 @@
-export default function getPort(options?:{
-	port?: number | number[],
+/**
+ * Options to configure how to find an available port.
+ */
+export type Options = Readonly<{
+	/**
+	 * A preferred port or an array of preferred ports to use.
+	 */
+	port?: number | ReadonlyArray<number>,
+
+	/**
+	 * The host on which port resolution should be performed. Can be either an IPv4 or IPv6 address.
+	 */
 	host?: string;
-}): Promise<number>;
+}>;
+
+/**
+ * Returns a `Promise` for an available port number.
+ *
+ * @param options
+ * @returns Available port number.
+ */
+export default function getPort(options?:Options): Promise<number>;
--- index.test-d.ts
+++ index.test-d.ts
@@ -1,7 +1,6 @@
 import {expectType} from 'tsd-check';
 import getPort from '.';
 
-
 expectType<number>(await getPort());
 expectType<number>(await getPort({port: 3000}));
 expectType<number>(await getPort({port: [3000, 3001, 3002]}));
