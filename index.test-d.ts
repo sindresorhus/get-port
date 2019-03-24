ffec5de63ef04fae8879ed72262e619c3cd87d17
Yanis Benson <yanis.benson@gmail.com>
2019-03-25 01:26:07 +0300

    clearer tests

--- index.test-d.ts
+++ index.test-d.ts
@@ -6,4 +6,4 @@ expectType<number>(await getPort({port: 3000}));
 expectType<number>(await getPort({port: [3000, 3001, 3002]}));
 expectType<number>(await getPort({host: 'https://localhost'}));
 
-expectType<number[]>(await getPort.makeRange(1025, 1026));
+expectType<number[]>(getPort.makeRange(1024, 1025));
--- test.js
+++ test.js
@@ -96,12 +96,12 @@ test('all preferred ports in array are unavailable', async t => {
 
 test('makeRange throws on invalid ranges', t => {
 	t.throws(() => getPort.makeRange(0, 0));
-	t.throws(() => getPort.makeRange(1024, 1024));
+	t.throws(() => getPort.makeRange(1023, 1023));
 	t.throws(() => getPort.makeRange(65536, 65536));
-	t.throws(() => getPort.makeRange(1026, 1025));
+	t.throws(() => getPort.makeRange(1025, 1024));
 });
 
 test('makeRange produces valid ranges', t => {
-	t.deepEqual(getPort.makeRange(1025, 1026), [1025]);
-	t.deepEqual(getPort.makeRange(1025, 1028), [1025, 1026, 1027]);
+	t.deepEqual(getPort.makeRange(1024, 1025), [1024]);
+	t.deepEqual(getPort.makeRange(1024, 1027), [1024, 1025, 1026]);
 });
