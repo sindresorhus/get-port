55bbfa48f00367a5893a8a366885d6e86805c28c
Sindre Sorhus <sindresorhus@gmail.com>
2019-12-16 19:40:14 +0100

    Update test.js

--- test.js
+++ test.js
@@ -129,24 +129,18 @@ test('makeRange produces valid ranges', t => {
 });
 
 test('ports are locked for up to 30 seconds', async t => {
-	// Speed up the test by overriding setInterval:
+	// Speed up the test by overriding `setInterval`.
 	const {setInterval} = global;
-	global.setInterval = (fn, timeout) => {
-		return setInterval(fn, timeout / 100);
-	};
+	global.setInterval = (fn, timeout) => setInterval(fn, timeout / 100);
 
 	delete require.cache[require.resolve('.')];
 	const getPort = require('.');
 	const timeout = promisify(setTimeout);
 	const port = await getPort();
-	const port2 = await getPort({
-		port
-	});
+	const port2 = await getPort({port});
 	t.not(port2, port);
 	await timeout(300); // 30000 / 100
-	const port3 = await getPort({
-		port
-	});
+	const port3 = await getPort({port});
 	t.is(port3, port);
 	global.setInterval = setInterval;
 });
