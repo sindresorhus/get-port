736b38ad186ebd08ca0732b91f6e6a7da7fe3c34
Sindre Sorhus <sindresorhus@gmail.com>
2022-02-16 16:35:09 +0700

    Update test.js

--- test.js
+++ test.js
@@ -142,7 +142,8 @@ test('makeRange produces valid ranges', t => {
 test('exclude produces ranges that exclude provided exclude list', async t => {
 	const exclude = [1024, 1026];
 	const foundPorts = await getPort({exclude, port: portNumbers(1024, 1026)});
-	// We should not find any of the exclusions in the foundPorts
+	
+	// We should not find any of the exclusions in `foundPorts`.
 	t.is(foundPorts, 1025);
 });
 
